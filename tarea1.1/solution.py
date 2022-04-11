3844881b1f288e6f425b7b8a180c2a256ad8bfa1
Susana Chang <samanta.chang@utec.edu.pe>
2022-04-11 15:12:13 -0500

    Add tarea1.1

---
+++ tarea1.1/solution.py
@@ -0,0 +1,58 @@
+# NO MODIFICAR NADA BAJO ESTA LINEA
+def main():
+# NO MODIFICAR NADA SOBRE ESTA LINEA
+
+    #============Pregunta 1============
+    N = int(input("N: "))
+
+    # Codigo para Pregunta 1 comienza aqui
+
+    print("")
+
+    # Codigo para Pregunta 1 acaba aqui
+
+    #============Pregunta 2============
+
+    N = int(input("N: "))
+
+    # Codigo para Pregunta 2 comienza aqui
+
+    print("")
+
+    # Codigo para Pregunta 2 acaba aqui
+    #============Pregunta 3============ 
+    
+    N = float(input("N: "))
+
+    # Codigo para Pregunta 3 comienza aqui
+
+    print("")
+
+    # Codigo para Pregunta 3 acaba aqui
+    
+
+    #============Pregunta 4============ 
+    
+    N = str(input("Nombre: "))
+
+    # Codigo para Pregunta 4 comienza aqui
+
+    print("")
+
+    # Codigo para Pregunta 4 acaba aqui
+
+    #============Pregunta 5============ 
+    
+    N = str(input("Animal: "))
+
+    # Codigo para Pregunta 5 comienza aqui
+
+    print("")
+
+    # Codigo para Pregunta 5 acaba aqui
+
+
+# NO MODIFICAR NADA BAJO ESTA LINEA
+if __name__ == '__main__':
+    main()
+# NO MODIFICAR NADA SOBRE ESTA LINEA
