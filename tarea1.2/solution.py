80e84f023c38ddc1c8f92a13d39a4f6bc38ab756
Susana Chang <samanta.chang@utec.edu.pe>
2022-04-23 14:02:06 -0500

    Add tarea1.2

---
+++ tarea1.2/solution.py
@@ -0,0 +1,40 @@
+# NO MODIFICAR NADA BAJO ESTA LINEA
+def main():
+    # NO MODIFICAR NADA SOBRE ESTA LINEA
+
+    # ============Pregunta 1============
+    promedio = float(input("promedio: "))
+    apellido = str(input("apellido: "))
+
+    # Codigo para Pregunta 1 comienza aqui
+
+
+
+    print("")
+    # Codigo para Pregunta 1 acaba aqui
+
+    # ============Pregunta 2============
+
+    votaciones = str(input("votación: "))
+
+    # Codigo para Pregunta 2 comienza aqui
+
+
+    print("")
+    # Codigo para Pregunta 2 acaba aqui
+
+    # ============Pregunta 3============
+
+    N = int(input("N: "))
+
+    # Codigo para Pregunta 3 comienza aqui
+
+
+    print("")
+    # Codigo para Pregunta 3 acaba aqui
+
+
+# NO MODIFICAR NADA BAJO ESTA LINEA
+if __name__ == '__main__':
+    main()
+# NO MODIFICAR NADA SOBRE ESTA LINEA
